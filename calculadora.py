988a287164b3d79491413a9b7aefd070e520cb47
MEspinaE <168482425+MEspinaE@users.noreply.github.com>
2024-07-16 12:18:40 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,9 @@
 def suma(a, b):
     return a + b
-
 def resta(a, b):
     return a - b
-
 def multiplicacion(a, b):
     return a * b
-
 def division(a, b):
     if b == 0:
         return "Error: División por cero"
